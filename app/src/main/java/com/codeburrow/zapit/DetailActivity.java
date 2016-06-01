ee6abb7fa423dfa2e7344624e6933a6725e316ea
George Spiridakis <spiridakis.grg@gmail.com>
2016-06-01 22:33:29 +0300

    Create DetailActivity. Resolve #2.

--- app/src/main/java/com/codeburrow/zapit/DetailActivity.java
+++ app/src/main/java/com/codeburrow/zapit/DetailActivity.java
@@ -5,15 +5,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.codeburrow.zapit.api.ZapItApi;
 import com.codeburrow.zapit.tasks.FindProductTask;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class DetailActivity extends AppCompatActivity implements FindProductTask.FindProductResponse {
 
     private static final String LOG_TAG = DetailActivity.class.getSimpleName();
 
-    private TextView mTextView;
+    private TextView mSlugTextView;
+    private TextView mNameTextView;
+    private TextView mPriceTextView;
+    private TextView mDescriptionTextView;
+    private TextView mPayedTextView;
+
     private String mNdefMessage;
 
     @Override
@@ -21,7 +28,11 @@ public class DetailActivity extends AppCompatActivity implements FindProductTask
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        mTextView = (TextView) findViewById(R.id.text_view);
+        mSlugTextView = (TextView) findViewById(R.id.detail_slug_textview);
+        mNameTextView = (TextView) findViewById(R.id.detail_name_textview);
+        mPriceTextView = (TextView) findViewById(R.id.detail_description_textview);
+        mDescriptionTextView = (TextView) findViewById(R.id.detail_price_textview);
+        mPayedTextView = (TextView) findViewById(R.id.detail_payed_textview);
 
         Bundle intentExtras = getIntent().getExtras();
         mNdefMessage = intentExtras != null ? intentExtras.getString(ScanNfcActivity.NDEF_MESSAGE_EXTRA) : null;
@@ -34,8 +45,21 @@ public class DetailActivity extends AppCompatActivity implements FindProductTask
 
     @Override
     public void onProcessFindProductFinish(JSONObject result) {
-        mTextView.setText(result.toString());
+        getProductDetailsFromJson(result);
 
         Log.e(LOG_TAG, result.toString());
     }
+
+    private void getProductDetailsFromJson(JSONObject productJson) {
+        try {
+            mSlugTextView.setText(productJson.getString(ZapItApi.SLUG));
+            mNameTextView.setText(productJson.getString(ZapItApi.NAME));
+            mPriceTextView.setText(productJson.getString(ZapItApi.PRICE));
+            mDescriptionTextView.setText(productJson.getString(ZapItApi.DESCRIPTION));
+            mPayedTextView.setText(String.format("%s", productJson.getBoolean(ZapItApi.PAYED)));
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, e.getMessage());
+        }
+    }
+
 }

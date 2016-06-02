eed54e0ca082f06b0365927956c3fca422eded51
George Spiridakis <spiridakis.grg@gmail.com>
2016-06-02 22:20:49 +0300

    Create PaymentActivity. Resolve #3.

--- app/src/main/java/com/codeburrow/zapit/PaymentActivity.java
+++ app/src/main/java/com/codeburrow/zapit/PaymentActivity.java
@@ -1,10 +1,13 @@
 package com.codeburrow.zapit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.codeburrow.zapit.api.ZapItApi;
 import com.codeburrow.zapit.tasks.GetPaymentStatusTask;
@@ -20,6 +23,10 @@ public class PaymentActivity extends AppCompatActivity implements GetPaymentStat
     private static final String LOG_TAG = PaymentActivity.class.getSimpleName();
 
     private Button mRequestPaymentButton;
+    private TextView mNamePurchaseTextView;
+    private TextView mDescriptionPurchaseTextView;
+    private ImageView mImageView;
+//    private ProgressDialog progressDialog;
 
     private String mProductSlug;
     private boolean mPayedStatus = false;
@@ -29,6 +36,9 @@ public class PaymentActivity extends AppCompatActivity implements GetPaymentStat
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
 
+        mNamePurchaseTextView = (TextView) findViewById(R.id.purchase_name_textview);
+        mDescriptionPurchaseTextView = (TextView) findViewById(R.id.purchase_description_textview);
+        mImageView = (ImageView) findViewById(R.id.imageview);
         mRequestPaymentButton = (Button) findViewById(R.id.request_payment_button);
         mRequestPaymentButton.setEnabled(mPayedStatus);
 
@@ -49,18 +59,34 @@ public class PaymentActivity extends AppCompatActivity implements GetPaymentStat
         }
     }
 
+    private void updateProductInformation(JSONObject result) {
+        try {
+            mNamePurchaseTextView.setText(result.getJSONObject(ZapItApi.DATA).getString(ZapItApi.NAME));
+            mDescriptionPurchaseTextView.setText(result.getJSONObject(ZapItApi.DATA).getString(ZapItApi.DESCRIPTION));
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, e.getMessage());
+        }
+    }
+
     public void requestPayment(View view) {
         new RequestPaymentTask(this, mProductSlug).execute();
+    }
+
+    public void requestScan(View view) {
+        Intent scanIntent = new Intent(this, ScanNfcActivity.class);
+        startActivity(scanIntent);
     }
 
     @Override
     public void onProcessGetPaymentStatusFinish(JSONObject result) {
         updatePayedStatus(result);
-
+        updateProductInformation(result);
 
         if (!mPayedStatus) {
             mRequestPaymentButton.setEnabled(!mPayedStatus);
             mRequestPaymentButton.setBackgroundResource(android.R.color.holo_green_light);
+        } else {
+            mImageView.setImageResource(R.drawable.not_available);
         }
 
         Log.e(LOG_TAG, "Value of mPayed: " + String.valueOf(mPayedStatus));
@@ -71,6 +97,7 @@ public class PaymentActivity extends AppCompatActivity implements GetPaymentStat
         updatePayedStatus(result);
 
         if (mPayedStatus) {
+            mImageView.setImageResource(R.drawable.completed);
             mRequestPaymentButton.setEnabled(!mPayedStatus);
             mRequestPaymentButton.setBackgroundResource(android.R.drawable.btn_default);
         }
